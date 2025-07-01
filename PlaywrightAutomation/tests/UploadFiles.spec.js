39ade2580614ebbebfaca33f7f7095743345b0fc
Etienne-Perso <etienne.im22@gmail.com>
2025-07-01 14:52:49 +0200

    Committing Upload files test file

--- PlaywrightAutomation/tests/UploadFiles.spec.js
+++ PlaywrightAutomation/tests/UploadFiles.spec.js
@@ -4,12 +4,29 @@ test("Upload single file", async ({page})=>{
 
     await page.goto("https://blueimp.github.io/jQuery-File-Upload/")
 
-    await page.locator("//input[@type='file']").click()
 
-    await page.locator("//input[@type='file']").setInputFiles("tests/uploadFiles/nature.jpg")
+    //await page.locator("//input[@type='file']").setInputFiles("tests/uploadFiles/nature.jpg")
+    await page.setInputFiles("//input[@type='file']", "tests/uploadFiles/nature.jpg")
 
-    await page.waitForTimeout(9000)
+    await page.waitForTimeout(3000)
+})
 
+test.only("Upload multiple files", async ({page})=>{
 
+    await page.goto("https://davidwalsh.name/demo/multiple-file-upload.php")
 
+    //Uploading the 2 files
+    await page.locator("#filesToUpload").setInputFiles(["tests/uploadFiles/nature.jpg","tests/uploadFiles/nature1.jpg"])
+
+    //check the field's file names
+    await expect(await page.locator("div[class='main'] li:nth-child(1)")).toHaveText("nature.jpg") 
+    await expect(await page.locator("div[class='main'] li:nth-child(2)")).toHaveText("nature1.jpg") 
+
+    await page.waitForTimeout(3000)
+
+    //Removing the files
+    await page.setInputFiles("#filesToUpload",[])
+    await expect(await page.locator("//li[normalize-space()='No Files Selected']")).toHaveText("No Files Selected")
+
+    await page.waitForTimeout(3000)    
 })
